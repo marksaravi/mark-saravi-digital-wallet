7d233d9d55f31c8e9dc0146fce9339b6a4a020ae
marksaravi <marksaravi@gmail.com>
2024-02-04 18:24:51 +1100

    transacyion history

--- packages/functions/src/wallet/list.ts
+++ packages/functions/src/wallet/list.ts
@@ -1,8 +1,20 @@
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      transactions: []
-    }),
-  };
-}
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import service from "@mark-saravi-digital-wallet/core/services/walletService";
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  const userId: string = (event?.pathParameters?.userid || "");
+  const response = service.getTransacgtionsHistory(userId)
+
+  return response.statusCode == 200
+    ? {
+      statusCode: 200,
+      body: JSON.stringify({
+        userId,
+        transactionHistory: response.transactionHistory
+      }),
+    }
+    : {
+      statusCode: 404,
+      body: JSON.stringify({}),
+    };
+};
